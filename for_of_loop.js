3a5febb4cd5dcab3b226a6c44f1c370ae162d786
Fumigatus <alpcan237@gmail.com>
2022-05-01 05:56:12 +0300

    for ... of loop

--- for_of_loop.js
+++ for_of_loop.js
@@ -3,3 +3,18 @@ const arr = ["Golf",2500,"John",()=>console.log("anon func")];
 for (let element of arr) {
   console.log(element);
 }
+console.log("-------")
+
+const arr2 = [10, 20, 30];
+
+for (let value of arr2) {
+  value ++;
+  console.log(value);
+}
+console.log("-------")
+
+const arr3 = new Map([['John', 1], ['Anna', 2], ['Smith', 3]]);
+
+for (const entry of arr3) {
+  console.log(entry);
+}
